0c90ce36c50f1c8c055ea8b222f27859b85cd192
Andre <andrejss88@gmail.com>
2020-05-04 17:25:10 +0200

    Add second UI test

--- src/test/java/com/framework/MyFirstUITest.java
+++ src/test/java/com/framework/MyFirstUITest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -33,6 +34,36 @@ public class MyFirstUITest {
         Assertions.assertEquals(user, actualUserName);
 
         driver.close();
+    }
+
+    @Test
+    void repoLinkGoesToCorrectRepo() {
+
+        // Arrange
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        String user = "andrejs-ps";
+        driver.get("https://github.com/" + user);
+
+
+        // Act
+        String repo = "automated-tests-in-java-with-fluent-interface-using-webdriver-selenium";
+        WebElement repoLink = driver.findElement(By.linkText(repo));
+
+        repoLink.click();
+
+        String actualUrl = driver.getCurrentUrl();
+
+
+        // Assert
+        Assertions.assertEquals("https://github.com/andrejs-ps/" + repo, actualUrl);
+
+        driver.close();
+
     }
 
 
