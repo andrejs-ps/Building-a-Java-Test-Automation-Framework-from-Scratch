f57b8069f40a148f15db062000fc7294ffef74e2
Andre <andrejss88@gmail.com>
2020-05-04 14:49:22 +0200

    Create a real test

--- src/test/java/com/framework/MyFirstTest.java
+++ src/test/java/com/framework/MyFirstTest.java
@@ -1,5 +1,6 @@
 package com.framework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MyFirstTest {
@@ -7,6 +8,12 @@ public class MyFirstTest {
 
     @Test
     void myFirstTest() {
-        System.out.println("Runs!");
+        int result = sum(2, 2);
+
+        Assertions.assertEquals(4 , result);
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
     }
 }
