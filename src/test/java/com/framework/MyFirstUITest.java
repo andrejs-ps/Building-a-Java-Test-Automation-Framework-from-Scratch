a1b485531489b8b2533af2d48eef8a45e5d2f287
Andre <andrejss88@gmail.com>
2020-05-04 16:27:16 +0200

    Add Selenium and the first UI test

---
+++ src/test/java/com/framework/MyFirstUITest.java
@@ -0,0 +1,39 @@
+package com.framework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyFirstUITest {
+
+
+    @Test
+    void userNameIsCorrectOnOverviewTab() {
+
+        // Arrange
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        String user = "andrejs-ps";
+        driver.get("https://github.com/" + user);
+
+        // Act
+        String actualUserName = driver.findElement(By.className("p-nickname")).getText();
+
+
+        // Assert
+        Assertions.assertEquals(user, actualUserName);
+
+        driver.close();
+    }
+
+
+}
